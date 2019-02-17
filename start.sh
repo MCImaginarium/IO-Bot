fb74d99ef2270bac1ff038c031f694ca2558f6a6
root <root@iocraft.org>
2019-02-17 11:57:44 -0600

    Bleh... automation and stuff.

--- start.sh
+++ start.sh
@@ -12,21 +12,21 @@ MONGO=$(cat ~/.mongo)
 cd "$(dirname "$0")"
 if ! screen -list | grep -q "bot"; then
 	echo "Bot was not started, starting in background!"
-	cd /storage/resist-discord-bot/
-	rm /storage/resist-discord-bot/config.json
-	rm /storage/resist-discord-bot/tps.sh
-	sed "s#MCR_PASS#$MCR_PASS#g" /storage/resist-discord-bot/tps.sh.template > /storage/resist-discord-bot/tps.sh;
-	sed "s#DISCORD_TOKEN#$DISCORD#g" /storage/resist-discord-bot/config.json.template > /storage/resist-discord-bot/config.json;
-	sed -i "s/MYSQL_USER/$MYSQL_USER/g" /storage/resist-discord-bot/config.json;
-	sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /storage/resist-discord-bot/config.json;
-	sed -i "s/MYSQL_HOST/$MYSQL_HOST/g" /storage/resist-discord-bot/config.json;
-	sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /storage/resist-discord-bot/config.json;
-	sed -i "s/YOUTUBE_KEY/$YOUTUBE_KEY/g" /storage/resist-discord-bot/config.json;
-	sed -i "s/GOOGLE_URL/$GOOGLE_URL/g" /storage/resist-discord-bot/config.json;
+	cd /storage/bot/
+	rm /storage/bot/config.json
+	rm /storage/bot/tps.sh
+	sed "s#MCR_PASS#$MCR_PASS#g" /storage/bot/tps.sh.template > /storage/bot/tps.sh;
+	sed "s#DISCORD_TOKEN#$DISCORD#g" /storage/bot/config.json.template > /storage/bot/config.json;
+	sed -i "s/MYSQL_USER/$MYSQL_USER/g" /storage/bot/config.json;
+	sed -i "s/MYSQL_PASSWORD/$MYSQL_PASSWORD/g" /storage/bot/config.json;
+	sed -i "s/MYSQL_HOST/$MYSQL_HOST/g" /storage/bot/config.json;
+	sed -i "s/MYSQL_DATABASE/$MYSQL_DATABASE/g" /storage/bot/config.json;
+	sed -i "s/YOUTUBE_KEY/$YOUTUBE_KEY/g" /storage/bot/config.json;
+	sed -i "s/GOOGLE_URL/$GOOGLE_URL/g" /storage/bot/config.json;
 	git stash;
 	git pull;
 	#npm update;
-        screen -LdmS bot node /storage/resist-discord-bot/bot.js
+        screen -LdmS bot node /storage/bot/bot.js
 else
 	echo "Bot is already started, not starting!"
 fi
