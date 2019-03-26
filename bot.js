4d70d04ba09868055abcba4e9d39e61f38f2bb09
io-craft <io_craft@protonmail.com>
2019-03-26 12:35:00 +0000

    Bleh... automation and stuff.

--- bot.js
+++ bot.js
@@ -610,8 +610,8 @@ const commands = {
     " Community! We host our own custom Minecraft modpack and server. Scifi, Futuristic, CyberPunk themed"+
     " tech only survival pack. \n\nPlease check our chat channels, or visit our website at the link below"+
     " for the mod list, launcher information,\nconnect information and much much more.```\n\nGet it Now!  "+
-    "[ https://www.ioCraft.org ]\n\nJoin <#515836198368051203> for regular chat and to meet the community!\n"+
-    "Use <#525842772247314442> for general help or open a ticket.\n\n**SERVER INFORMATION**```css\nModded"+
+    "[ https://www.ioCraft.org ]\n\nJoin <#544564718849753120> for regular chat and to meet the community!\n"+
+    "Use <#560079070248042505> for general help or open a ticket.\n\n**SERVER INFORMATION**```css\nModded"+
     " Minecraft Version { 1.12.2 }\nServer Stats { DEDICATED, 16GB RAM, SSD RAID, 8 CORE }\n```\n"+
     "**OPEN A SUPPORT TICKET** - **!open**\n```ldif\nCreates a private channel and staff will assist you when"+
     " they are available.\n\nFor immediate help, check out or other channels, or check out site for various"+
